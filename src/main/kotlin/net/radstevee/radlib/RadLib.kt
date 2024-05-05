7eb3f41f7594ce60976e02eb1925467647fe309a
radsteve <radsteve@radsteve.net>
2024-05-05 15:29:09 +0200

    Make `plugin` internal and add a logger

--- src/main/kotlin/net/radstevee/radlib/RadLib.kt
+++ src/main/kotlin/net/radstevee/radlib/RadLib.kt
@@ -3,9 +3,10 @@ package net.radstevee.radlib
 import org.bukkit.plugin.java.JavaPlugin
 
 object RadLib {
-    lateinit var plugin: JavaPlugin
+    internal lateinit var plugin: JavaPlugin
 
     fun initialize(plugin: JavaPlugin) {
+        plugin.logger.info("RadLib - Initialized!")
         this.plugin = plugin
     }
 }
